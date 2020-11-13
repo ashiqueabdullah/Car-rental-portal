8372c22d74417b5b46d55430b7aae0721f3ceabf
ashiqueabdullah <ashiqueashique465@gmail.com>
2020-11-13 23:16:56 +0600

    header top done without responsive

--- index.php
+++ index.php
@@ -7,19 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Online car portal</title>
 
-
-
     <!--  font awesome icons  -->
     <link rel="stylesheet" href="./css/all.min.css">
 
-
-    <!--  Owl-carousel css file  -->
-    <link rel="stylesheet" href="./vendor/owl-carousel/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="./vendor/owl-carousel/css/owl.theme.default.min.css">
-
-
     <!--  custom css file  -->
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <!--  Responsive css file  -->
     <link rel="stylesheet" href="./css/responsive.css">
@@ -32,7 +24,37 @@
     <!--  ======================= Start Header Area ============================== -->
 
     <div class="header_area">
-        
+        <div class="container">
+            <div class="logo">
+                <img src="img/logg.png" alt="">
+            </div>
+            <div class="foremail">
+                <div class="left">
+                    <i class="far fa-envelope"></i>
+                </div>
+                <div class="right">
+                    <h4>FOR SUPPORT MAIL US :</h4>  <p><a href="mailto:name@email.com">codeprojectsorg@gmail.com</a></p>
+                </div>
+            </div>
+            <div class="forphone">
+                <div class="left">
+                    <i class="fas fa-phone"></i>
+                </div>
+                <div class="right">
+                    <h4>SERVICE HELPLINE CALL US:</h4>  <p><a href="">+91-9876543210</a></p>
+                </div>
+            </div>
+            <div class="forsocial">
+                <ul>
+                    <li><a href="#"> <i class="fab fa-facebook-square"></i></a></li>
+                    <li><a href="#"> <i class="fab fa-twitter-square"></i></a></li>
+                    <li><a href="#"> <i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="#"> <i class="fab fa-instagram-square"></i></a></li>
+                    <li><a href="#"> <i class="fab fa-google-plus-square"></i></a></li>
+                    <li class="reglog"><a href="#">Login/Register</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 
 
@@ -41,10 +63,6 @@
 
     <!--  Jquery js file  -->
     <script src="./js/jquery.3.4.1.js"></script>
-
-
-    <!--  Owl-carousel js file  -->
-    <script src="./vendor/owl-carousel/js/owl.carousel.min.js"></script>
 
     <!--  custom js file  -->
     <script src="./js/main.js"></script>
